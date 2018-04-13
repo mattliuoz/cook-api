d592f30ff22cbb86ce0fc810b76558eb68556758
Matt Liu <mattliu@Matts-MacBook-Pro.local>
2018-04-13 16:18:10 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+### Setup
+
+npm install -g aws-sam-local  
+
+npm install -g serverless  
+
+### Local Dev
+
+To run api at local, use commannd  ```serverless offline start```   
+
+### Deployment
+To kick off deployment locally:   
+1) Assume role in aws that has all required access to deploy  
+2) use command ```serverless deploy ```  
---
+++ create.js
@@ -0,0 +1,95 @@
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const uuid = require('uuid');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const env = process.env.ENV;
+const dynamodbTableName = process.env.DYNAMODB_TABLE
+//Get env specific config
+const config = require('./config/config');
+
+const buildBadRequestResponse = function buildBadRequestResponse(message, requestId) {
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            message: message,
+            requestId: requestId
+        }),
+        isBase64Encoded: false
+    };
+}
+const buildInternalErrorResponse = function buildInternalErrorResponse(message, requestId) {
+    return {
+        statusCode: 500,
+        body: JSON.stringify({
+            message: message,
+            requestId: requestId
+        }),
+        isBase64Encoded: false
+    }
+}
+
+exports.handler = async (event, context, callback) => {
+    try {
+        const timestamp = new Date().getTime();
+
+        const data = JSON.parse(event.body);
+
+        if (typeof data.ingredients !== 'string') {
+            const response = buildBadRequestResponse("ingredients is not string", context.requestId);
+            callback(null, response);
+            return;
+        }
+
+        if (typeof data.method !== 'string') {
+            const response = buildBadRequestResponse("method is not string", context.requestId);
+            callback(null, response);
+            return;
+        }
+
+        if (typeof data.duration !== 'string') {
+            const response = buildBadRequestResponse("duration is not string", context.requestId);
+            callback(null, response);
+            return;
+        }
+
+        if (typeof data.logo !== 'string') {
+            const response = buildBadRequestResponse("logo is not string", context.requestId);
+            callback(null, response);
+            return;
+        }
+
+        const payload = {
+            TableName: dynamodbTableName,
+            Item: {
+                id: uuid.v1(),
+                ingredients: data.ingredients,
+                method: data.method,
+                duration: data.duration,
+                logo: data.logo,
+                createdAt: timestamp,
+                updatedAt: timestamp,
+            },
+        };
+
+
+        const cb = (err, data) => {
+            if (err) {
+                const response = buildInternalErrorResponse(err, context.requestId);
+                context.done(null, response);
+            } else {
+                const response = {
+                    isBase64Encoded: false,
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        'message': JSON.stringify(payload.Item),
+                        'input': event,
+                    }),
+                };
+                context.done(null, response);
+            }
+        }
+        dynamo.put(payload, cb);
+    } catch (error) {
+        const response = buildInternalErrorResponse(error);
+        context.done(null, response);
+    }
+};
